06b3c6d729f78bc4733c084ea77e11000ce5e6d5
Marcio Silva <marcioatlondon@gmail.com>
2017-03-15 20:54:00 +0000

    Initial Commit

---
+++ GuessTheNumber.py
@@ -0,0 +1,25 @@
+# This is a guess the number game
+#Importing ramdom package
+import random
+
+#Assignings ramdom value from 1 to 20 to variable secretNumber
+secreteNumber = random.randint(1,20)
+print("Guess a number between 1 and 20:")
+
+# Ask the player to guess 6 times
+for guessTaken in range(1,7):
+  print ("Take a guess:")
+  guess = int(input())
+
+  if guess < secreteNumber:
+    print("Your guess was too low")
+  elif guess > secreteNumber:
+    print("Your guess was too high")
+  else:
+    break    # This condition is the correct guess!
+
+#Testing for correct guess
+if guess == secreteNumber:
+  print("Good job, " + str(guess) + " was the correct number.")
+else:
+  print("Noooo, the correct number was " + str(guess) + ".")
