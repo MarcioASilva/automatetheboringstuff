d8670b08f1e4fbb157b49d8eb1104a7c75d1c095
Marcio Silva <marcioatlondon@gmail.com>
2017-03-28 23:33:39 +0100

    finished magical8Ball.py

--- magic8Ball.py
+++ magic8Ball.py
@@ -1,16 +1,15 @@
 import random
-def getAnswer(answerNumber):
-  messages = ['It is certain',
-              'It is decidedly so',
-              'Yes definitely',
-              'Reply hazy try again',
-              'Ask again later',
-              'Concentrate and ask again',
-              'My reply is no',
-              'Outlook not so good',
-              'Very doubtful'
-              ]
+messages = ['It is certain',
+            'It is decidedly so',
+            'Yes definitely',
+            'Reply hazy try again',
+            'Ask again later',
+            'Concentrate and ask again',
+            'My reply is no',
+            'Outlook not so good',
+            'Very doubtful'
+            ]
 
-r = random.randint(1, 9)
-fortune = getAnswer(messages[r])
+r = random.randint(0, 8)
+fortune = messages[r]
 print(fortune)
