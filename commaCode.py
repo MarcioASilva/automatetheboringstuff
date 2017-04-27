2e3a29c209669c78c5d2b3c4c1789a792bc49873
Marcio Silva <marcioatlondon@gmail.com>
2017-04-27 12:24:05 +0100

    Removing commaCode.py

--- commaCode.py
+++
@@ -1,6 +0,0 @@
-spam = ['apples', 'bananas', 'tofu', 'cats']
-
-def function(listParameter):
-  for item in listParameter:
-    print(listParameter[item])
-  # return a string with all the items separated by a comma and a space
